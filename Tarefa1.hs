e631ac94b3741d38b0cea2021b2671c01c0b2aee
Francisco Mota <franciscoo.mota28@gmail.com>
2023-05-30 15:31:31 +0100

    update

--- Tarefa1.hs
+++ Tarefa1.hs
@@ -4,6 +4,22 @@ ser apresentado um aviso no terminal caso o número de salas e dias disponíveis
 insuficientes para acomodar todos os exames necessários, sendo que não poderá ocorrer 
 mais que um exame na mesma sala no mesmo dia (5 valores).-}
 
+--import System.IO(hPutStrLn)
+import System.IO ( hClose, hPutStrLn, openFile, IOMode(WriteMode) )
+
+tarefa1 :: IO ()
+tarefa1 = do
+    putStrLn "Introduza o número total de dias em que os exames podem ocorrer: "
+    dias <- getLine
+    putStrLn "Introduza o número total de salas disponíveis: "
+    salas <- getLine
+    escreveFicheiro "exemplo.txt" (dias ++ " " ++ salas)
+
+escreveFicheiro :: FilePath -> String -> IO ()
+escreveFicheiro nomeArquivo conteudo = do
+    arquivo <- openFile nomeArquivo WriteMode
+    hPutStrLn arquivo conteudo
+    hClose arquivo
 
 
 
